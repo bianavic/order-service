7fdceecb47e38de074930d299b3e73076409c006
bianavic <bianavic@gmail.com>
2021-11-02 10:56:26 -0300

    created a query to update user balance

--- src/main/java/com/udemy/orderservice/repository/UserRepository.java
+++ src/main/java/com/udemy/orderservice/repository/UserRepository.java
@@ -1,9 +1,21 @@
 package com.udemy.orderservice.repository;
 
 import com.udemy.orderservice.entity.User;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface UserRepository extends ReactiveCrudRepository<User, Integer> {
+
+  @Modifying // chain this annotation, add
+  @Query("update user " +
+      "set balance = balance - :amount " +
+      "where id - :userId " +
+      "and balance >= :amount"
+  )
+  Mono<Boolean> updateUserBalance(int userId, int amount);
+
 }
