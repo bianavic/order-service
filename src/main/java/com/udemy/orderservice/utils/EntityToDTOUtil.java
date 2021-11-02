cf8554e0b733db100367b1d79af09488d6194579
bianavic <bianavic@gmail.com>
2021-11-02 11:04:15 -0300

    created a transaction dto util

--- src/main/java/com/udemy/orderservice/utils/EntityToDTOUtil.java
+++ src/main/java/com/udemy/orderservice/utils/EntityToDTOUtil.java
@@ -1,7 +1,13 @@
 package com.udemy.orderservice.utils;
 
+import com.udemy.orderservice.dto.TransactionRequestDTO;
+import com.udemy.orderservice.dto.TransactionResponseDTO;
+import com.udemy.orderservice.dto.TransactionStatus;
 import com.udemy.orderservice.dto.UserDTO;
 import com.udemy.orderservice.entity.User;
+import com.udemy.orderservice.entity.UserTransaction;
+import java.time.LocalDateTime;
+import javax.print.attribute.standard.OrientationRequested;
 import org.springframework.beans.BeanUtils;
 
 public class EntityToDTOUtil {
@@ -16,6 +22,25 @@ public class EntityToDTOUtil {
     User user = new User();
     BeanUtils.copyProperties(dto, user);
     return user;
+  }
+
+  // request
+  public static UserTransaction toEntity(TransactionRequestDTO requestDTO) {
+    UserTransaction userTransaction = new UserTransaction();
+    BeanUtils.copyProperties(requestDTO, userTransaction);
+    userTransaction.setUserId(requestDTO.getUserId());
+    userTransaction.setAmount(requestDTO.getAmount());
+    userTransaction.setTransactionDate(LocalDateTime.now());
+    return userTransaction;
+  }
+
+  // response
+  public static TransactionResponseDTO toDTO(TransactionRequestDTO requestDTO, TransactionStatus status) {
+    TransactionResponseDTO responseDTO = new TransactionResponseDTO();
+    responseDTO.setUserId(requestDTO.getUserId());
+    responseDTO.setAmount(requestDTO.getAmount());
+    responseDTO.setStatus(status);
+    return responseDTO;
   }
 
 }
