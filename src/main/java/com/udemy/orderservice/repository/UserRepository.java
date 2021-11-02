828b28cc4c30594f4428c21dede768376ce32e74
bianavic <bianavic@gmail.com>
2021-11-02 10:13:57 -0300

    created repositories

---
+++ src/main/java/com/udemy/orderservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.udemy.orderservice.repository;
+
+import com.udemy.orderservice.entity.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends ReactiveCrudRepository<User, Integer> {
+}
---
+++ src/main/java/com/udemy/orderservice/repository/UserTransactionRepository.java
@@ -0,0 +1,9 @@
+package com.udemy.orderservice.repository;
+
+import com.udemy.orderservice.entity.UserTransaction;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserTransactionRepository extends ReactiveCrudRepository<UserTransaction, Integer> {
+}
