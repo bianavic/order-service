c991368cd5346e0452dc26e9aa78f6d6abb4fbce
bianavic <bianavic@gmail.com>
2022-01-09 17:05:44 -0300

    handling webclient exceptions with bad request or service unavailable

--- src/main/java/com/udemy/orderservice/controller/PurchaseOrderController.java
+++ src/main/java/com/udemy/orderservice/controller/PurchaseOrderController.java
@@ -3,7 +3,9 @@ package com.udemy.orderservice.controller;
 import com.udemy.orderservice.dto.*;
 import com.udemy.orderservice.service.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.*;
 import reactor.core.publisher.*;
 
 @RestController
@@ -17,8 +19,11 @@ public class PurchaseOrderController {
   private OrderQueryService queryService;
 
   @PostMapping
-  public Mono<PurchaseOrderResponseDTO> order(@RequestBody Mono<PurchaseOrderRequestDTO> requestDTOMono) {
-    return this.orderFulfillmentService.processOrder(requestDTOMono);
+  public Mono<ResponseEntity<PurchaseOrderResponseDTO>> order(@RequestBody Mono<PurchaseOrderRequestDTO> requestDTOMono) {
+    return this.orderFulfillmentService.processOrder(requestDTOMono)
+        .map(ResponseEntity::ok)
+        .onErrorReturn(WebClientResponseException.class, ResponseEntity.badRequest().build()) // webclient response request
+        .onErrorReturn(WebClientRequestException.class, ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build()); // not able to connect to other services
   }
 
   // expose endpoint to get list of orders for a particular user
